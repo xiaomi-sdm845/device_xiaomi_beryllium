347a1fb0b4533a26af68bc239273d0d590386639
Bruno Martins <bgcngm@gmail.com>
2019-01-11 23:04:15 +0000

    beryllium: Add device-specific fstab and flags
     * This got decommonized in order to support perseus,
       which is a system-as-root device.
    
    Change-Id: I48b0477e60e7c0cdcf1fba746639d442e2edf6af

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -28,6 +28,12 @@ BOARD_HAVE_QCOM_FM := true
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_CONFIG := beryllium_defconfig
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/beryllium/BoardConfigVendor.mk
